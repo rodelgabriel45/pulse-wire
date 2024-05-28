366af3c34fa94750584d455f97595be570911fd2
rodelgabriel45@gmail.com <rodelgabriel45@gmail.com>
2024-05-28 15:42:31 +0800

    change to path resolve in sending html file - pre deployment

--- server/index.js
+++ server/index.js
@@ -35,7 +35,7 @@ if (process.env.NODE_ENV === "production")
   app.use(express.static(path.join(__dirname, "client/dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+  res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
 });
 
 // Error handling middleware
