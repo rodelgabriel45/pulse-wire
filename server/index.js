820fdebfcaac126e76e07244a10502e674f9e06f
rodelgabriel45@gmail.com <rodelgabriel45@gmail.com>
2024-05-28 15:46:05 +0800

    fix the server pre deployment

--- server/index.js
+++ server/index.js
@@ -32,10 +32,10 @@ app.use("/api/notification", notificationRoutes);
 
 // Deployment
 if (process.env.NODE_ENV === "production")
-  app.use(express.static(path.join(__dirname, "client/dist")));
+  app.use(express.static(path.join(__dirname, "/client/dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
 });
 
 // Error handling middleware
